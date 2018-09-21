50b91f02967b08ae463f131aa8d6c53e91e9ac89
pablodiegoss <pablodiegoss@hotmail.com>
2018-09-21 17:22:17 -0300

    Create a Test Suit for CDB tests

---
+++ src/tests/TestSuitCDBCalc.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	TestRendimentoBruto.class,
+	TestImpostoDeRenda.class,
+	TestRendimentoLiquido.class
+})
+
+public class TestSuitCDBCalc {
+	
+}
