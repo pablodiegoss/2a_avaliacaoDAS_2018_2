d67337fe072c8a761e95ec7c06d5157f249b77b9
pablodiegoss <pablodiegoss@hotmail.com>
2018-09-21 16:59:54 -0300

    Create a new third test for Imposto de Renda

--- src/tests/TestImpostoDeRenda.java
+++ src/tests/TestImpostoDeRenda.java
@@ -22,4 +22,11 @@ public class TestImpostoDeRenda {
 		double taxaDeJuros = 8;
 		assertEquals(2.96, CDBCalc.calcularImpostoDeRenda(dias,aplicacao,taxaDeJuros),0.005);
 	}
+	@Test
+	public void testImposto3() {
+		int dias = 240;
+		double aplicacao = 3000.0;
+		double taxaDeJuros = 9;
+		assertEquals(35.51, CDBCalc.calcularImpostoDeRenda(dias,aplicacao,taxaDeJuros),0.005);
+	}
 }
