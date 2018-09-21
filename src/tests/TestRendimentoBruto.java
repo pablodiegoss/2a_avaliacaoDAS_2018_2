a876e8fbd0bd0493cd86a271e773353113d5b617
pablodiegoss <pablodiegoss@hotmail.com>
2018-09-21 16:37:02 -0300

    Create third test for Rendimento Bruto

--- src/tests/TestRendimentoBruto.java
+++ src/tests/TestRendimentoBruto.java
@@ -21,4 +21,12 @@ public class TestRendimentoBruto {
 		double taxaDeJuros = 8.0;
 		assertEquals(13.15, CDBCalc.calcularRendimentoBruto(dias,aplicacao,taxaDeJuros), 0.005);
 	}
+	
+	@Test
+	public void testrb3() {
+		int dias = 240;
+		double aplicacao = 3000.0;
+		double taxaDeJuros = 9.0;
+		assertEquals(177.53, CDBCalc.calcularRendimentoBruto(dias,aplicacao,taxaDeJuros), 0.005);
+	}
 }
