c5c178397167d68b148ebb0fa16437c35b93b12c
pablodiegoss <pablodiegoss@hotmail.com>
2018-09-21 16:47:11 -0300

    Improve tests for Rendimento bruto

--- src/tests/TestRendimentoBruto.java
+++ src/tests/TestRendimentoBruto.java
@@ -15,12 +15,20 @@ import code.CDBCalc;
 @RunWith(Parameterized.class)
 public class TestRendimentoBruto {
 	
-	@Parameters
+	@Parameters(name="index = dias= {0}, aplicacao= {1}, taxa= {2}, resultado={3}")
 	public static Collection<Object[]> data(){
 		return Arrays.asList(new Object[][] {
 			{ 60, 1000.0, 8.5, 13.97},
 			{ 120, 500.0, 8.0, 13.15},
-			{ 240, 3000.0, 9.0, 177.53}
+			{ 240, 3000.0, 9.0, 177.53},
+			{ 270, 2000.0, 8.5, 125.75},
+			{ 390, 100.0, 7.5, 8.01},
+			{ 420, 250.0, 8.0, 23.01},
+			{ 550, 400.0, 8.0, 48.22},
+			{ 670, 800.0, 8.0, 117.48},
+			{ 700, 2500.0, 9.0, 431.51},
+			{ 900, 4200.0, 9.5, 983.84},
+			{ 1000, 100.0, 7.5, 20.55},
 		});
 	}
 	
