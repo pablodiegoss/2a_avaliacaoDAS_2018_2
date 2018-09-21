b9e9433d5bdc847364abd8b4323c096c428222d1
pablodiegoss <pablodiegoss@hotmail.com>
2018-09-21 16:44:37 -0300

    Parameterize Test for Rendimento Bruto

--- src/tests/TestRendimentoBruto.java
+++ src/tests/TestRendimentoBruto.java
@@ -1,32 +1,43 @@
 package tests;
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import code.CDBCalc;
 
+@RunWith(Parameterized.class)
 public class TestRendimentoBruto {
-	@Test
-	public void testrb1() {
-		int dias = 60;
-		double aplicacao = 1000.0;
-		double taxaDeJuros = 8.5;
-		assertEquals(13.97, CDBCalc.calcularRendimentoBruto(dias,aplicacao,taxaDeJuros), 0.005);
+	
+	@Parameters
+	public static Collection<Object[]> data(){
+		return Arrays.asList(new Object[][] {
+			{ 60, 1000.0, 8.5, 13.97},
+			{ 120, 500.0, 8.0, 13.15},
+			{ 240, 3000.0, 9.0, 177.53}
+		});
 	}
 	
-	@Test
-	public void testrb2() {
-		int dias = 120;
-		double aplicacao = 500.0;
-		double taxaDeJuros = 8.0;
-		assertEquals(13.15, CDBCalc.calcularRendimentoBruto(dias,aplicacao,taxaDeJuros), 0.005);
-	}
+	@Parameter
+	public int dias;
+	
+	@Parameter(1)
+	public double aplicacao;
+
+	@Parameter(2)
+	public double taxa;
+
+	@Parameter(3)
+	public double rendimento;
 	
 	@Test
-	public void testrb3() {
-		int dias = 240;
-		double aplicacao = 3000.0;
-		double taxaDeJuros = 9.0;
-		assertEquals(177.53, CDBCalc.calcularRendimentoBruto(dias,aplicacao,taxaDeJuros), 0.005);
+	public void testMethod() {
+		assertEquals(rendimento, CDBCalc.calcularRendimentoBruto(dias, aplicacao, taxa), 0.005);
 	}
 }
