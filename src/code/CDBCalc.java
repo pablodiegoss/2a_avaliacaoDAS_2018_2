c56d2688da778bc8bf9454b3fcee85b411f8f0fb
pablodiegoss <pablodiegoss@hotmail.com>
2018-09-21 16:35:24 -0300

    Implement Rendimento Bruto

--- src/code/CDBCalc.java
+++ src/code/CDBCalc.java
@@ -3,6 +3,8 @@ package code;
 public class CDBCalc {
 
 	public static double calcularRendimentoBruto(int dias, double aplicacao, double taxa) {
-		return 13.97;
+		double anos = dias/365.0;
+		return aplicacao * (taxa/100 * anos);
 	}
+	
 }
