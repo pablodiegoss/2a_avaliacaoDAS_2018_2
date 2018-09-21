077b13377e02cee893acc25326a19198b0bf6816
pablodiegoss <pablodiegoss@hotmail.com>
2018-09-21 17:08:33 -0300

    Create new Test for Rendimento Liquido

--- src/tests/TestRendimentoLiquido.java
+++ src/tests/TestRendimentoLiquido.java
@@ -9,10 +9,17 @@ import code.CDBCalc;
 public class TestRendimentoLiquido {
 	
 	@Test
-	public void testImposto1() {
+	public void testRendimento1() {
 		int dias = 60;
 		double aplicacao = 1000.0;
 		double taxaDeJuros = 8.5;
 		assertEquals(1.0829, CDBCalc.calcularRendimento(dias,aplicacao,taxaDeJuros),0.00005);
+	}
+	@Test
+	public void testRendimento2() {
+		int dias = 120;
+		double aplicacao = 500.0;
+		double taxaDeJuros = 8;
+		assertEquals(2.0384, CDBCalc.calcularRendimento(dias,aplicacao,taxaDeJuros),0.00005);
 	}
 }
